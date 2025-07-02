11f0d59f54a1a6bed8cb55ece13ccfd88a2c3092
Trevor-Kayeyia-Madara <trevor.madara@student.moringaschool.com>
2025-07-02 21:21:30 +0300

    Flask App Initialization

---
+++ school_management_backend/app/__init__.py
@@ -0,0 +1,25 @@
+# type: ignore
+
+from flask import Flask 
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from dotenv import load_dotenv
+import os
+
+db = SQLAlchemy()
+
+def create_app():
+    load_dotenv()
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+    CORS(app)
+
+    # Import routes
+    from app.routes import init_routes
+    init_routes(app)
+
+    return app
