306d367146688091f1cbfa4abd0bb4ba12750abc
Trevor-Kayeyia-Madara <trevor.madara@student.moringaschool.com>
2025-07-02 23:18:22 +0300

    Auth Utils

--- school_management_backend/routes/__init__.py
+++ school_management_backend/routes/__init__.py
@@ -1,6 +1,8 @@
 # type: ignore
 from flask import Blueprint
 from .auth import auth_bp
+from .users import user_bp
 
 def init_routes(app):
     app.register_blueprint(auth_bp, url_prefix='/api/v1/auth')
+    app.register_blueprint(user_bp, url_prefix="/api/v1/users")
