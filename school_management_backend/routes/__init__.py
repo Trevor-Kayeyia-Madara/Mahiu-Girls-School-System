6e9fb1fe3086450f5545f209b35700c259bb7bee
Trevor-Kayeyia-Madara <trevor.madara@student.moringaschool.com>
2025-07-02 23:05:12 +0300

    Authentication worked

--- school_management_backend/routes/__init__.py
+++ school_management_backend/routes/__init__.py
@@ -1,6 +1,6 @@
 # type: ignore
 from flask import Blueprint
+from .auth import auth_bp
 
 def init_routes(app):
-    from .auth import auth_bp
     app.register_blueprint(auth_bp, url_prefix='/api/v1/auth')
--- school_management_backend/routes/auth.py
+++ school_management_backend/routes/auth.py
@@ -1,14 +1,12 @@
-#type:ignore
+# type: ignore
 from flask import Blueprint, request, jsonify
 from models import User
 from app import db
 import jwt
 import datetime
 import os
-from flask_bcrypt import Bcrypt
 
 auth_bp = Blueprint('auth', __name__)
-bcrypt = Bcrypt()
 
 @auth_bp.route('/login', methods=['POST'])
 def login():
@@ -18,10 +16,7 @@ def login():
 
     user = User.query.filter_by(email=email).first()
 
-    if not user:
-        return jsonify({'error': 'Invalid credentials'}), 401
-
-    if not bcrypt.check_password_hash(user.password, password):
+    if not user or user.password != password:
         return jsonify({'error': 'Invalid credentials'}), 401
 
     token = jwt.encode({
