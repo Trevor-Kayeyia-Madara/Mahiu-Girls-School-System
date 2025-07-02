211bfe9c36b3afa7c7ddc0d8825d043da3d24793
Trevor-Kayeyia-Madara <trevor.madara@student.moringaschool.com>
2025-07-02 21:28:10 +0300

    Auth Initialize

---
+++ school_management_backend/routes/auth.py
@@ -0,0 +1,8 @@
+# type: ignore
+from flask import Blueprint, jsonify
+
+auth_bp = Blueprint('auth', __name__)
+
+@auth_bp.route('/ping', methods=['GET'])
+def ping():
+    return jsonify({"message": "Auth route working!"})
