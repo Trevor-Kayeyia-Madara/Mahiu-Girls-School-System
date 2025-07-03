60765cf66b9042b7f06331ae8ecdee046efe0f33
Trevor-Kayeyia-Madara <trevor.madara@student.moringaschool.com>
2025-07-03 12:25:31 +0300

    subjects crude

--- school_management_backend/routes/__init__.py
+++ school_management_backend/routes/__init__.py
@@ -4,9 +4,12 @@ from .auth import auth_bp
 from .users import user_bp
 from .students import student_bp
 from .staff import staff_bp
+from .subjects import subject_bp
+
 
 def init_routes(app):
     app.register_blueprint(auth_bp, url_prefix='/api/v1/auth')
     app.register_blueprint(user_bp, url_prefix="/api/v1/users")
     app.register_blueprint(student_bp, url_prefix="/api/v1/students")
     app.register_blueprint(staff_bp, url_prefix='/api/v1/staff')
+    app.register_blueprint(subject_bp, url_prefix='/api/v1/subjects')
