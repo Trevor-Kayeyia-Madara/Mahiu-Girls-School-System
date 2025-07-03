565260e68f2f1163cc5a63ac5080c47f1e261e7f
Trevor-Kayeyia-Madara <trevor.madara@student.moringaschool.com>
2025-07-03 15:47:40 +0300

    First commit of frontend

---
+++ school-frontend/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center text-xl font-semibold">
+      School Management System Frontend Ready 🚀
+    </div>
+  )
+}
+
+export default App
