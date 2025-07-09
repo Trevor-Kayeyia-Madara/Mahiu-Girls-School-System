3d28b07a030f8be4fc8b7d96a019af09f8824755
Trevor-Kayeyia-Madara <trevor.madara@student.moringaschool.com>
2025-07-09 19:54:40 +0300

    Stage 1: Project Structure & Setup

---
+++ school_management_backend/app/__init__.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_cors import CORS
+from dotenv import load_dotenv
+import os
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+def create_app():
+    load_dotenv()
+
+    app = Flask(__name__)
+    app.config.from_object('config.Config')
+
+    CORS(app)
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    from routes import init_routes
+    init_routes(app)
+
+    return app
---
+++ school_management_backend/routes/__init__.py
@@ -0,0 +1,3 @@
+def init_routes(app):
+    # We will register blueprints here in later stages
+    pass
