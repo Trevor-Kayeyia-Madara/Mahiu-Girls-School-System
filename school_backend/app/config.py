2126845516352d7c5755fabac4be04dee3a8d2a7
Trevor-Kayeyia-Madara <trevor.madara@student.moringaschool.com>
2025-07-09 16:58:27 +0300

    Python setup

--- school_backend/app/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'dev_secret_key')
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///school.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY', 'jwt_secret')
