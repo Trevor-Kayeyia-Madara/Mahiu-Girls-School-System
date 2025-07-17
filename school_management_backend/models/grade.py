e8a476051d9da4b2960d15b1ccfdd2b5fcafb193
Trevor-Kayeyia-Madara <trevormadarakayeyia@gmail.com>
2025-07-17 14:40:35 +0300

    Fix grade

--- school_management_backend/models/grade.py
+++ school_management_backend/models/grade.py
@@ -1,9 +1,8 @@
-from datetime import datetime
 from app import db
 
 class Grade(db.Model):
     __tablename__ = 'grades'
     grade_id = db.Column(db.Integer, primary_key=True)
-    student_id = db.Column(db.Integer, nullable=False)
     exam_schedule_id = db.Column(db.Integer, nullable=False)
     marks = db.Column(db.Float, nullable=False)
+    student_id = db.Column(db.Integer, db.ForeignKey('students.student_id'), nullable=False)
